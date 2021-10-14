7e66e27bd7b29f2ab762181540ac9fe65dbd7803
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-10-14 12:15:26 +0530

    Update vaman6.sh

--- vaman6.sh
+++ vaman6.sh
@@ -1,2 +1,4 @@
 cd /data/data/com.termux/files/home/pygmy-dev/tools/vtr-verilog-to-routing
 make -j4
+cp /data/data/com.termux/files/home/pygmy-dev/tools/vtr-verilog-to-routing/build/vpr/vpr $INSTALL_DIR/bin
+cp /data/data/com.termux/files/home/pygmy-dev/tools/vtr-verilog-to-routing/build/utils/fasm/genfasm $INSTALL_DIR/bin
