4e629cf7788afc37ba4e7525528e32ed0c456f26
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-10-14 12:48:36 +0530

    Update vaman2.sh

--- vaman2.sh
+++ vaman2.sh
@@ -1,5 +1,5 @@
 mkdir -p /data/data/com.termux/files/home/symbiflow
 export  INSTALL_DIR=/data/data/com.termux/files/home/symbiflow
-wget http://fast.kjaisai1337.workers.dev/arch.tar.gz?rootId=root
+wget http://fast.kjaisai1337.workers.dev/arch.tar.gz?rootId=root -O arch.tar.gz
 tar -C $INSTALL_DIR  -xvf /data/data/com.termux/files/home/arch.tar.gz
 export PATH="$INSTALL_DIR/quicklogic-arch-defs/bin:$INSTALL_DIR/quicklogic-arch-defs/bin/python3:$PATH"
