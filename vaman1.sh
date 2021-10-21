dbc01af8ec77647936b20201bfef71140f5813ce
jaisai1337 <48624947+jaisai1337@users.noreply.github.com>
2021-10-21 10:00:03 +0530

    Update vaman1.sh

--- vaman1.sh
+++ vaman1.sh
@@ -1,2 +1,2 @@
-apt update & apt upgrade -y
+apt update && apt upgrade -y
 apt install openssh-server sshpass build-essential libssl-dev libffi-dev python3-dev bison flex git tcl-dev tcl tcl-tclreadline libreadline-dev  autoconf libtool make automake texinfo pkg-config libusb-1.0-0 libusb-1.0-0-dev gcc-arm-none-eabi libnewlib-arm-none-eabi telnet python3 apt-utils libxslt-dev python3-lxml python3-simplejson cmake curl  python3-setuptools python3-pip -y
